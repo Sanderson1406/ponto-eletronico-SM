e829a9af2e2142ce2f1fe7cb7b0040ad20eaea61
Sanderson1406 <sandersonoficial10@hotmail.com>
2024-08-27 10:45:16 -0300

    "continuação"

--- js/index.js
+++ js/index.js
@@ -1,17 +1,35 @@
 const diaSemana= document.getElementById("dia-semana");
 const diaMesAno= document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
-diaSemana.textContent = "Domingo poha";
+diaSemana.textContent = getWeekDay();
+horaMinSeg.textContent = getCurrentHour();
 diaMesAno.textContent = getCurrentDate();
 
 function getCurrentDate() {
-    const date = new Data();
-    const dia = date.getDate();
-    const mes = date.getMonth();
-    return (parseInt(dia)) + "/" + (parseInt(mes) + 1) + "/" + date.getFullTear();
+    const date = new Date();
+    let dia = date.getDate();
+    let mes = date.getMonth();
+    if (dia < 10) {
+        dia = "0" + dia
+    }
+    return dia + "/" + (parseInt(mes) + 1) + "/" + date.getFullTear();
+}
+
+function getCurrentHour() {
+    const hour = new Date();
+    return hour.getHours() + ":" + hour.getMinutes() + ":" + hour.getSeconds();
+}
+
+function getWeekDay() {
+    const dia = new Date();
+    let days = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feria", "Quinta-Feira", "Sexta-Feria", "Sabado"]
+    return days[dia.getDay()]
 }
 
 
-console.log(date.getDay());
-console.log(date.getDate());
-console.log(date.getMonth());
+const btnBaterPonto = document.getElementById("btn-bater-ponto")
+btnBaterPonto.addEventListener("click", register())
+
+function register() {
+    alert("Bater ponto")
+}
